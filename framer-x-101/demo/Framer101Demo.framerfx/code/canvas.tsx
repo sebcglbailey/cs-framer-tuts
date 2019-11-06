75e14978d45db59cd0bcf8131afc5c7045d75aa7
Seb Bailey <sebastian@clearscore.com>
2019-11-06 16:46:52 +0000

    removes old folder demo file

--- framer-x-101/demo/Framer101Demo.framerfx/code/canvas.tsx
+++
@@ -1,4 +0,0 @@
-// WARNING: this file is auto generated, any changes will be lost
-import { createDesignComponent, CanvasStore } from "framer"
-const canvas = CanvasStore.shared(); // CANVAS_DATA;
-export const Card = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string,Title?:string,Description?:string}>(canvas, "id_H38u_hg4g", {Title:"string",Description:"string"}, 327,203);
